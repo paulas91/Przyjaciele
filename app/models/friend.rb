4ffe4745839a0b615426c305e26c56c30a373717
paulas91 <paulinaskopp@gmail.com>
2022-11-08 12:38:43 +0100

    Add validates to Friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Friend < ApplicationRecord
+  validates :first_name, :last_name, :email, :residence, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A[\w\.\-]+[@]\w+[.][a-zA-Z]{2,4}\z/,
+  message: "format is incorect" }
+  
   def full_name
     "#{first_name} #{last_name}"
   end
+
 end
