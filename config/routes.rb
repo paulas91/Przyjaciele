8f4d9e00c147734f678adc3b6d40a665bb7a68b5
paulas91 <paulinaskopp@gmail.com>
2022-11-11 16:56:37 +0100

    Addition of 4 subpages and scope.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@
 
 Rails.application.routes.draw do
   root 'friends#index'
-  resources :friends
+  resources :friends do
+    collection do
+      get :school
+      get :work
+      get :party
+      get :holiday
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
