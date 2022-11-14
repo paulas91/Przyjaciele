6baf2c6fb5fff2868afa52aa0982721e96a68144
paulas91 <paulinaskopp@gmail.com>
2022-11-14 20:21:36 +0100

    Describe, context, it/test

---
+++ spec/rspec_methods_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+describe Friend do
+  it 'creates friend with atributes' do
+    friend = described_class.create(first_name: 'Ola',
+                                    last_name: 'Lola',
+                                    residence: 'Kraków',
+                                    cognition: described_class.cognitions[:school])
+    expect(friend.first_name).to eq 'Ola'
+  end
+
+  context 'when first name not passed' do
+    it 'does not create friend' do
+      friend = described_class.create(first_name: '',
+                                       last_name: 'Lola',
+                                       residence: 'Kraków',
+                                       cognition: described_class.cognitions[:school])
+      expect(described_class.count).to eq 0
+    end
+  end
+
+  it 'is not valid' do
+    friend = described_class.new(first_name: '')
+    expect(friend.valid?).to eq false
+  end
+end
