04b97ee408b5a31665819a91643e641595d5a777
paulas91 <paulinaskopp@gmail.com>
2022-11-14 21:48:36 +0100

    let i let! in RSpec

--- spec/subject_described_class_spec.rb
+++ spec/subject_described_class_spec.rb
@@ -14,18 +14,22 @@ end
 
 describe MyLogger do
   subject(:logger) { described_class.new }
+  let(:message1) { 'Hello World' }
+  let(:message2) { 'Ruby is awesome' }
 
   it 'logs a message' do
-    logged_message = logger.log('Hello World')
+    logged_message = logger.log(message1)
     expect(logged_message).to include 'Hello World'
   end
 
-  it 'adds message to messages array' do
-    message1 = 'Hello World'
-    message2 = 'Ruby is awesome'
-    logger.log(message1)
-    logger.log(message2)
-    expect(logger.messages).to include message1
-    expect(logger.messages).to include message2
+  context 'for logging' do
+    let(:message1) { 'Other message'}
+
+    it 'adds message to messages array' do
+      logger.log(message1)
+      logger.log(message2)
+      expect(logger.messages).to include message1
+      expect(logger.messages).to include message2
+    end
   end
 end
