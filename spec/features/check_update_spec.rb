bb1fbe5cdad6d89464c412dc45e41b8fe15566e7
paulas91 <paulinaskopp@gmail.com>
2022-11-20 13:31:56 +0100

    Update friend test

---
+++ spec/features/check_update_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+describe 'Update friend' do
+  let(:friend) { create(:friend) }
+  let(:new_attributes) { attributes_for(:friend) }
+
+  before do
+    friend
+    visit "/friends/#{friend.id}"
+  end
+
+  it 'update friend' do
+    expect(page).to have_content(friend.residence)
+    click_link 'Edit this friend'
+
+    expect(page).to have_content('Editing friend')
+    fill_in 'friend_first_name',	with: new_attributes[:first_name]
+    fill_in 'friend_last_name',	with: new_attributes[:last_name]
+    fill_in 'friend_email',	with: new_attributes[:email]
+    fill_in 'friend_residence', with: new_attributes[:residence]
+    select 'party', from: 'friend[cognition]'
+    click_button 'Update Friend'
+
+    expect(page).to have_content('Friend was successfully updated.')
+
+    email = page.find('#email').text
+    residence = page.find('#residence').text
+    cognition = page.find('#cognition').text
+    expect(find('h5')).to have_content("#{new_attributes[:last_name]} #{new_attributes[:first_name]}")
+    expect(email).to eq "email: #{new_attributes[:email]}"
+    expect(residence).to eq "residence: #{new_attributes[:residence]}"
+    expect(cognition).to eq 'cognition: party'
+    click_link 'Back to friends'
+
+    expect(page).to have_content("#{new_attributes[:last_name]} #{new_attributes[:first_name]}")
+    expect(page).to have_content('party')
+  end
+end
