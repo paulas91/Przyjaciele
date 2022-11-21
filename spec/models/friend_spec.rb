b3dde3950505a4208d354ac982e0fe7ae2eaba74
paulas91 <paulinaskopp@gmail.com>
2022-11-21 09:55:56 +0100

    Add devise gem.

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: friends
+#
+#  id         :bigint           not null, primary key
+#  cognition  :integer          default("other"), not null
+#  email      :string
+#  first_name :string
+#  last_name  :string
+#  residence  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_friends_on_email  (email) UNIQUE
+#
 describe Friend, type: :model do
   describe 'columns' do
     it { is_expected.to have_db_column(:first_name).of_type(:string) }
